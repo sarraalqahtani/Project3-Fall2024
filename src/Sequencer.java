85bcb8da9b801b18ac6c9b40e310fa8426f6fc48
alqahtas <alqahtas@wfu.edu>
2024-09-18 13:05:53 -0400

    Added Collection.sort

--- src/Sequencer.java
+++ src/Sequencer.java
@@ -1,6 +1,9 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
 public class Sequencer {
     public static void main(String[] args) {
-
+        //these 2 lines can be used to sort the list of words
+        ArrayList<String> eWords = new ArrayList<>();
+        Collections.sort(eWords);
     }
 }
